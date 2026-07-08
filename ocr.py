1c5ec869b523cc4ac73f67f5d1e50ba7e9c507c3
moon <jenishpant137@gmail.com>
2026-07-08 13:47:44 +0545

    restructure

--- ocr.py
+++
@@ -1,3 +0,0 @@
-pip install torch transformers pymupdf
-model = AutoModel.from_pretrained(“baidu/Unlimited-OCR”, trust_remote_code=True)
-model.infer_multi(tokenizer, prompt=“<image>Multi page parsing.”, image_files=pages)
